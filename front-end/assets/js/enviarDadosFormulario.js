4b8bc758ea556f48d4ee86d4a2bd78a051212c35
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-05-22 20:10:48 -0300

    Alterações na home da aplicação e adição do carrinho

--- front-end/assets/js/enviarDadosFormulario.js
+++ front-end/assets/js/enviarDadosFormulario.js
@@ -39,3 +39,51 @@ function enviarCliente() {
     })
 }
 
+function validarCadastro() {
+    let nome = document.getElementById("nome").value;
+    let cpf = document.getElementById("cpf").value;
+    let email = document.getElementById("email").value;
+    let endereco = document.getElementById("endereco").value;
+    let telefone = document.getElementById("telefone").value;
+    let senha = document.getElementById("senha").value;
+    let confirmarSenha = document.getElementById("confirmar_senha").value;
+
+    if (nome == "") {
+        alert("Nome não informado");
+        nome.focus();
+        return;
+    } if (cpf == "") {
+        alert("CPF não informado");
+        cpf.focus();
+        return;
+
+    }
+    if (email == "") {
+        alert("E-mail não informado");
+        email.focus();
+        return;
+
+    } if (endereco == "") {
+        alert("Endereço não informado");
+        endereco.focus();
+        return;
+    } if (telefone == "") {
+        alert("Telefone não informado");
+        telefone.focus();
+        return;
+    }
+    if (senha == "") {
+        alert("Senha não informada");
+        senha.focus();
+        return;
+    } if (confirmarSenha == "") {
+        alert("Confirmar senha não informado");
+        confirmarSenha.focus();
+        return;
+    } if (senha != confirmarSenha) {
+        alert("As senhas não correspondem. Por favor, tente novamente.");
+        return;
+    }
+}
+
+
